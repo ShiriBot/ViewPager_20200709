b662dda9e3c58fcc8039ec94289158fb838d8276
채정실 <cowjdtlf@gmail.com>
2020-07-09 21:15:38 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/viewpager_20200709/MainActivity.kt
@@ -0,0 +1,19 @@
+package kr.co.tjoeun.viewpager_20200709
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+
+    }
+
+    override fun setupValues() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
